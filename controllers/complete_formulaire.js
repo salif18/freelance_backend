8f2f4db0fb1876dc2c967d58cc6ed64ebbbf75f0
salif18 <salifmoctarkonate@gmail.com>
2025-03-12 13:11:29 +0000

    changement de fonction update de user

--- controllers/complete_formulaire.js
+++ controllers/complete_formulaire.js
@@ -1,13 +1,20 @@
-// controllers/complete_formulaire.js
 const User = require('../models/user_model');
 
 exports.completInfos = async (req, res) => {
   try {
     const { userId } = req.params;
-    const user = await User.findByIdAndUpdate(userId, req.body, { new: true });
+
     if (!user) {
       return res.status(404).json({ message: "Utilisateur non trouvé." });
     }
+
+    // Mettre à jour uniquement les champs fournis dans req.body
+    const user = await User.findByIdAndUpdate(
+      userId,
+      { $set: req.body }, // Utilise $set pour mettre à jour uniquement les champs spécifiés
+      { new: true, runValidators: true } // Retourne le document mis à jour et exécute les validateurs
+    );
+
     res.status(200).json({ message: "Profil mis à jour avec succès", user });
   } catch (error) {
     res.status(500).json({ message: "Erreur lors de la mise à jour des informations.", error: error.message });
